e79e14c1259034f0066d01afb5051cb4e16c4b55
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-10-04 20:51:06 +0200

    b_develop_employee_payment: dodanie klas Employee i Payment do modelu

--- src/main/java/com/example/Motorola_HR_app/model/Employee.java
+++ src/main/java/com/example/Motorola_HR_app/model/Employee.java
@@ -1,4 +1,32 @@
 package com.example.Motorola_HR_app.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.pl.PESEL;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Imię nie może być puste")
+    private String firstName;
+    @NotBlank(message = "Nazwisko nie może być puste")
+    private String lastName;
+    @NotBlank
+    @PESEL
+    private String pesel;
+    private String address;
+    @NotBlank
+    private String position;
+    private String description;
+    @ManyToOne
+    private Payment payment;
+
+
 }
