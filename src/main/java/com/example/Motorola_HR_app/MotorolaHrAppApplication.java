54540a3d3d0b7ccd8d8632858810074e6dc58563
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-10-04 20:29:27 +0200

    initial commit

---
+++ src/main/java/com/example/Motorola_HR_app/MotorolaHrAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Motorola_HR_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorolaHrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorolaHrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Motorola_HR_app/MotorolaHrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Motorola_HR_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorolaHrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
